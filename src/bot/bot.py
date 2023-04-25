6ea52480ce6592fe42c642f27ed3ad8be056d4b0
playferkeeps <buddy.cryptocurrency@gmail.com>
2023-04-24 23:47:55 -0500

    update bot

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -24,7 +24,7 @@ async def on_message(message):
         return
     if '{}'.format(message.channel.id) != '{}'.format(CHANNEL_TO_COMM_IN):
         return
-    if message.content.startswith('$getcred'):
+    if message.content.startswith('$sendSPA'):
         await message.channel.send('Checking CRED balance, standby...')
         arggecknum = message.content.split()[1]
         arggecknum = re.sub('[^A-Za-z0-9]+', '', arggecknum)
@@ -43,40 +43,7 @@ async def on_message(message):
         if not found:
             await message.channel.send('We\'re Sorry. Geck {} has no CRED :('.format(arggecknum))
         # await message.channel.send(geckostats[1])
-    if message.content.startswith('$geckinfo'):
-        await message.channel.send('Looking up Gecko, standby...')
-        arggecknum = message.content.split()[1]
-        arggecknum = re.sub('[^A-Za-z0-9]+', '', arggecknum)
-        geckostats = requests.get('https://galacticgeckos-ni5j6e99z-ggsg-team.vercel.app/api/stats').json()
-        gecko = geckometadata[arggecknum]
-        tokenAddress = gecko['TokenAddress']
-        found = False
-        for journeyinfo in geckostats['stakeEntryListReadable']:
-                if journeyinfo['originalMint'] == tokenAddress:
-                    print('FOUND! {}'.format(tokenAddress))
-                    heliusurl = "https://api.helius.xyz/v1/nft-events?api-key={}".format(config['HELIUS_TOKEN'])
-                    found = True
-                    credBalance = journeyinfo['totalPoints'] /100
-                    if journeyinfo['amount'] != 1:
-                        journeyduration = "Not on Journey, SEND IT!"
-                    else:
-                        journeyduration = journeyinfo['lockPeriodNum']
-                    geckoMessage = '```Name: Galactic Gecko #{} \nCRED Balance: {}\nToken Address: {}\nJourney Duration: {}```'.format(gecko['Number'], credBalance, tokenAddress, journeyduration)
-                    async with aiohttp.ClientSession() as session: # creates session
-                        async with session.get(gecko['Image']) as resp: # gets image from url
-                            img = await resp.read() # reads image from response
-                            with io.BytesIO(img) as file: # converts to file-like object
-                                await message.channel.send(file=discord.File(file, 'gecko.png'))
-                    await message.channel.send(geckoMessage)
-                    break
-        if not found:
-            geckoMessage = '```Name: Galactic Gecko #{} \nCRED Balance: {}\nToken Address: {}\nJourney Duration: {}```'.format(gecko['Number'], "0", tokenAddress, "0")
-            async with aiohttp.ClientSession() as session: # creates session
-                async with session.get(gecko['Image']) as resp: # gets image from url
-                    img = await resp.read() # reads image from response
-                    with io.BytesIO(img) as file: # converts to file-like object
-                        await message.channel.send(file=discord.File(file, 'gecko.png'))
-            await message.channel.send(geckoMessage)
+    
     if message.content.startswith('$geckobot'):
         arg = message.content.split()[1]
         if '{}'.format(arg) == 'help':
